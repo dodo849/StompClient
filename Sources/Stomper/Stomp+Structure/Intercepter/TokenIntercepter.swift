32c948c995164e8e523818c67d6d8a11c1949693
leedoyeon849 <do83430208@gmail.com>
2024-06-19 14:07:39 +0900

    fix: Update access modifier of TokenIntercepter

--- Sources/Stomper/Stomp+Structure/Intercepter/TokenIntercepter.swift
+++ Sources/Stomper/Stomp+Structure/Intercepter/TokenIntercepter.swift
@@ -10,6 +10,10 @@ import Foundation
 public struct TokenIntercepter: Intercepter {
     let token: String
     
+    public init(token: String) {
+        self.token = token
+    }
+    
     public func intercept<E: EntryType>(_ entry: E) -> E {
         let tokenHeader = ["Authorization": "Bearer \(token)"]
         entry.headers.addHeaders(tokenHeader)
