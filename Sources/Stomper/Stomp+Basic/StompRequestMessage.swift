159652555623e558621a2d1895c89d7144e8bb27
leedoyeon849 <do83430208@gmail.com>
2024-06-20 23:33:30 +0900

    feat:Remove Stomp Request message protocol.

--- Sources/Stomper/Stomp+Basic/StompRequestMessage.swift
+++ Sources/Stomper/Stomp+Basic/StompRequestMessage.swift
@@ -21,13 +21,30 @@ public enum StompRequestCommand: String {
 }
 
 
-public protocol StompRequestMessage {
-    var command: StompRequestCommand { get }
-    var headers: StompHeaders { get }
-    var body: StompBody? { get }
+public struct StompRequestMessage {
+    public let command: StompRequestCommand
+    public let headers: StompHeaders
+    public var body: StompBody? = nil
     
-    func toFrame() -> String
+    init(
+        command: StompRequestCommand,
+        headers: [String: String],
+        body: StompBody?
+    ) {
+        self.command = command
+        self.headers = StompHeaders(headers)
+        self.body = body
+    }
 }
+
+
+//public protocol StompRequestMessage {
+//    var command: StompRequestCommand { get }
+//    var headers: StompHeaders { get }
+//    var body: StompBody? { get }
+//    
+//    func toFrame() -> String
+//}
 
 extension StompRequestMessage {
     public func toFrame() -> String {
@@ -63,200 +80,183 @@ extension StompRequestMessage {
         return frame
     }
 }
-
-public struct StompAnyMessage: StompRequestMessage {
-    public let command: StompRequestCommand
-    public let headers: StompHeaders
-    public var body: StompBody? = nil
-    
-    init(
-        command: StompRequestCommand,
-        headers: [String: String],
-        body: StompBody?
-    ) {
-        self.command = command
-        self.headers = StompHeaders(headers)
-        self.body = body
-    }
-}
-
-struct StompConnectMessage: StompRequestMessage {
-    let command: StompRequestCommand = .connect
-    let headers: StompHeaders
-    var body: StompBody? = nil
-    
-    init(
-        accecptVersion: String = "1.2",
-        host: String
-    ) {
-        let headers = [
-            "accept-version": accecptVersion,
-            "host": host
-        ]
-        self.headers = StompHeaders(headers)
-    }
-    
-    init(headers: [String: String]) {
-        self.headers = StompHeaders(headers)
-    }
-}
-
-struct StompSubscribeMessage: StompRequestMessage {
-    let command: StompRequestCommand = .subscribe
-    let headers: StompHeaders
-    let body: StompBody? = nil
-    
-    init(
-        id: String,
-        destination: String
-    ) {
-        let headers = [
-            "id" : id,
-            "destination": destination
-        ]
-        self.headers = StompHeaders(headers)
-    }
-    
-    init(headers: [String: String]) {
-        self.headers = StompHeaders(headers)
-    }
-}
-
-struct StompSendMessage: StompRequestMessage {
-    let command: StompRequestCommand = .send
-    let headers: StompHeaders
-    let body: StompBody?
-    
-    init(
-        destination: String,
-        body: StompBody?
-    ) {
-        let headers = {
-            if let body = body {
-                return [
-                    "destination": destination,
-                    "content-type": body.contentType,
-                    "content-length": "\(body.contentLength)"
-                ]
-            } else {
-                return ["destination": destination]
-            }
-        }()
-        self.headers = StompHeaders(headers)
-        self.body = body
-    }
-    
-    init(
-        headers: [String: String],
-        body: StompBody? = nil
-    ) {
-        self.headers = StompHeaders(headers)
-        self.body = body
-    }
-}
-
-struct StompDisconnectMessage: StompRequestMessage {
-    let command: StompRequestCommand = .disconnect
-    let headers: StompHeaders
-    let body: StompBody? = nil
-    
-    init(receipt: String? = nil) {
-        if let receipt = receipt {
-            self.headers = StompHeaders(["receipt": receipt])
-        } else {
-            self.headers = StompHeaders([:])
-        }
-    }
-    
-    init(headers: [String: String]) {
-        self.headers = StompHeaders(headers)
-    }
-}
-
-
-// 🔽 Not used yet
-struct StompNackMessage: StompRequestMessage {
-    let command: StompRequestCommand = .nack
-    let headers: StompHeaders
-    let body: StompBody? = nil
-    
-    init(id: String, transaction: String? = nil) {
-        var headers = ["id": id]
-        if let transaction = transaction {
-            headers["transaction"] = transaction
-        }
-        self.headers = StompHeaders(headers)
-    }
-    
-    init(headers: [String: String]) {
-        self.headers = StompHeaders(headers)
-    }
-}
-
-struct StompUnsubscribeMessage: StompRequestMessage {
-    let command: StompRequestCommand = .unsubscribe
-    let headers: StompHeaders
-    let body: StompBody? = nil
-    
-    init(id: String) {
-        let headers = [
-            "id": id
-        ]
-        self.headers = StompHeaders(headers)
-    }
-    
-    init(headers: [String: String]) {
-        self.headers = StompHeaders(headers)
-    }
-}
-
-struct StompBeginMessage: StompRequestMessage {
-    let command: StompRequestCommand = .begin
-    let headers: StompHeaders
-    let body: StompBody? = nil
-    
-    init(transaction: String) {
-        let headers = [
-            "transaction": transaction
-        ]
-        self.headers = StompHeaders(headers)
-    }
-    
-    init(headers: [String: String]) {
-        self.headers = StompHeaders(headers)
-    }
-}
-
-struct StompCommitMessage: StompRequestMessage {
-    let command: StompRequestCommand = .commit
-    let headers: StompHeaders
-    let body: StompBody? = nil
-    
-    init(transaction: String) {
-        let headers = [
-            "transaction": transaction
-        ]
-        self.headers = StompHeaders(headers)
-    }
-    
-    init(headers: [String: String]) {
-        self.headers = StompHeaders(headers)
-    }
-}
-
-struct StompAbortMessage: StompRequestMessage {
-    let command: StompRequestCommand = .abort
-    let headers: StompHeaders
-    let body: StompBody? = nil
-    
-    init(transaction: String) {
-        let headers = [
-            "transaction": transaction
-        ]
-        self.headers = StompHeaders(headers)
-    }
-    
-    init(headers: [String: String]) {
-        self.headers = StompHeaders(headers)
-    }
-}
+//struct StompConnectMessage: StompRequestMessage {
+//    let command: StompRequestCommand = .connect
+//    let headers: StompHeaders
+//    var body: StompBody? = nil
+//    
+//    init(
+//        accecptVersion: String = "1.2",
+//        host: String
+//    ) {
+//        let headers = [
+//            "accept-version": accecptVersion,
+//            "host": host
+//        ]
+//        self.headers = StompHeaders(headers)
+//    }
+//    
+//    init(headers: [String: String]) {
+//        self.headers = StompHeaders(headers)
+//    }
+//}
+//
+//struct StompSubscribeMessage: StompRequestMessage {
+//    let command: StompRequestCommand = .subscribe
+//    let headers: StompHeaders
+//    let body: StompBody? = nil
+//    
+//    init(
+//        id: String,
+//        destination: String
+//    ) {
+//        let headers = [
+//            "id" : id,
+//            "destination": destination
+//        ]
+//        self.headers = StompHeaders(headers)
+//    }
+//    
+//    init(headers: [String: String]) {
+//        self.headers = StompHeaders(headers)
+//    }
+//}
+//
+//struct StompSendMessage: StompRequestMessage {
+//    let command: StompRequestCommand = .send
+//    let headers: StompHeaders
+//    let body: StompBody?
+//    
+//    init(
+//        destination: String,
+//        body: StompBody?
+//    ) {
+//        let headers = {
+//            if let body = body {
+//                return [
+//                    "destination": destination,
+//                    "content-type": body.contentType,
+//                    "content-length": "\(body.contentLength)"
+//                ]
+//            } else {
+//                return ["destination": destination]
+//            }
+//        }()
+//        self.headers = StompHeaders(headers)
+//        self.body = body
+//    }
+//    
+//    init(
+//        headers: [String: String],
+//        body: StompBody? = nil
+//    ) {
+//        self.headers = StompHeaders(headers)
+//        self.body = body
+//    }
+//}
+//
+//struct StompDisconnectMessage: StompRequestMessage {
+//    let command: StompRequestCommand = .disconnect
+//    let headers: StompHeaders
+//    let body: StompBody? = nil
+//    
+//    init(receipt: String? = nil) {
+//        if let receipt = receipt {
+//            self.headers = StompHeaders(["receipt": receipt])
+//        } else {
+//            self.headers = StompHeaders([:])
+//        }
+//    }
+//    
+//    init(headers: [String: String]) {
+//        self.headers = StompHeaders(headers)
+//    }
+//}
+//
+//
+//// 🔽 Not used yet
+//struct StompNackMessage: StompRequestMessage {
+//    let command: StompRequestCommand = .nack
+//    let headers: StompHeaders
+//    let body: StompBody? = nil
+//    
+//    init(id: String, transaction: String? = nil) {
+//        var headers = ["id": id]
+//        if let transaction = transaction {
+//            headers["transaction"] = transaction
+//        }
+//        self.headers = StompHeaders(headers)
+//    }
+//    
+//    init(headers: [String: String]) {
+//        self.headers = StompHeaders(headers)
+//    }
+//}
+//
+//struct StompUnsubscribeMessage: StompRequestMessage {
+//    let command: StompRequestCommand = .unsubscribe
+//    let headers: StompHeaders
+//    let body: StompBody? = nil
+//    
+//    init(id: String) {
+//        let headers = [
+//            "id": id
+//        ]
+//        self.headers = StompHeaders(headers)
+//    }
+//    
+//    init(headers: [String: String]) {
+//        self.headers = StompHeaders(headers)
+//    }
+//}
+//
+//struct StompBeginMessage: StompRequestMessage {
+//    let command: StompRequestCommand = .begin
+//    let headers: StompHeaders
+//    let body: StompBody? = nil
+//    
+//    init(transaction: String) {
+//        let headers = [
+//            "transaction": transaction
+//        ]
+//        self.headers = StompHeaders(headers)
+//    }
+//    
+//    init(headers: [String: String]) {
+//        self.headers = StompHeaders(headers)
+//    }
+//}
+//
+//struct StompCommitMessage: StompRequestMessage {
+//    let command: StompRequestCommand = .commit
+//    let headers: StompHeaders
+//    let body: StompBody? = nil
+//    
+//    init(transaction: String) {
+//        let headers = [
+//            "transaction": transaction
+//        ]
+//        self.headers = StompHeaders(headers)
+//    }
+//    
+//    init(headers: [String: String]) {
+//        self.headers = StompHeaders(headers)
+//    }
+//}
+//
+//struct StompAbortMessage: StompRequestMessage {
+//    let command: StompRequestCommand = .abort
+//    let headers: StompHeaders
+//    let body: StompBody? = nil
+//    
+//    init(transaction: String) {
+//        let headers = [
+//            "transaction": transaction
+//        ]
+//        self.headers = StompHeaders(headers)
+//    }
+//    
+//    init(headers: [String: String]) {
+//        self.headers = StompHeaders(headers)
+//    }
+//}
